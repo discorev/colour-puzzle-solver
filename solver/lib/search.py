702ba5780f48a4e7f44db3c8e3df0e8ded80a9c8
Ollie Hayman <discorev@olliespage.net>
2020-12-10 18:26:21 +0000

    Refactor search into it's own file

---
+++ solver/lib/search.py
@@ -0,0 +1,50 @@
+"""Implementation of search algorithms."""
+import logging
+from typing import List
+from collections import namedtuple
+from lib.collection import ContainerCollection
+
+Option = namedtuple("Option", ["collection", "moves"])
+
+
+def bfs(root: ContainerCollection) -> Option:
+    """Perform a Breadth-first search to find an optimal solution."""
+    # Ensure the search is required
+    if root.is_solved:
+        return Option(root, tuple())
+    queue: List[Option] = [Option(root.after(move), (move,)) for move
+                           in root.get_moves()]
+    while len(queue) > 0:
+        logging.debug(f"loop {len(queue[0].moves)} Options {len(queue)}")
+        next_queue: List[Option] = []
+        discovered: List[ContainerCollection] = []
+        for option in queue:
+            for move in option.collection.get_moves():
+                # If this move is the reverse of the previous move and the move
+                # before that was this move, a loop has been found and needs to
+                # be broken by simply ignoring this move.
+                if (
+                    len(option.moves) > 1
+                    and option.moves[-1] == move.reverse()
+                    and option.moves[-2] == move
+                ):
+                    continue
+                _next: ContainerCollection = option.collection.after(move)
+                is_solved = _next.is_solved
+                # If this is a leaf node, we cannot continue our search
+                if not is_solved and len(_next.get_moves()) == 0:
+                    continue
+                # Check if this option has been marked as visited
+                if _next in discovered:
+                    continue
+                moves = list(option.moves)
+                moves.append(move)
+                # Check if a solution was found
+                if is_solved:
+                    return Option(_next, tuple(moves))
+                discovered.append(_next)
+                next_queue.append(Option(_next, tuple(moves)))
+        queue = next_queue
+
+    # No valid options
+    return Option(root, tuple())
--- solver/solver.py
+++ solver/solver.py
@@ -1,53 +1,13 @@
 """Solve the water sort puzzel."""
-from collections import namedtuple
 import levels
 from lib.collection import ContainerCollection
+from lib.search import Option, bfs
 
-Option = namedtuple("Option", ["collection", "moves"])
-
-start = ContainerCollection(levels.level_160["start"])
+start = ContainerCollection(levels.stats["start"])
 print(start)
 
-options = [Option(start.after(move), (move,)) for move in start.get_moves()]
+result: Option = bfs(start)
 
-solved = False
-while not solved:
-    print("loop", len(options[0].moves), "Options", len(options))
-    next_options = []
-    next_collections = []
-    for option in options:
-        if solved:
-            break
-
-        for move in option.collection.get_moves():
-            # If this move is the reverse of the previous move and the move
-            # before that was this move, a loop has been found and needs to
-            # be broken by simply ignoring this move.
-            if (
-                len(option.moves) > 1
-                and option.moves[-1] == move.reverse()
-                and option.moves[-2] == move
-            ):
-                continue
-            _next: ContainerCollection = option.collection.after(move)
-            is_solved = _next.is_solved
-            # If this is a leaf node, we cannot continue our search
-            if not is_solved and len(_next.get_moves()) == 0:
-                continue
-            # Check if this option has been marked as visited
-            if _next in next_collections:
-                continue
-            moves = list(option.moves)
-            moves.append(move)
-            # Check if a solution was found
-            if is_solved:
-                next_options = [Option(_next, tuple(moves))]
-                solved = True
-                break
-            next_collections.append(_next)
-            next_options.append(Option(_next, tuple(moves)))
-    options = next_options
-
-print("solved in", len(options[0][1]), "moves")
-print(options[0][0])
-print(options[0][1])
+print("solved in", len(result.moves), "moves")
+print(result.collection)
+print(result.moves)
