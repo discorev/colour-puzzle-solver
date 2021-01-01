0e3fc164b9aaa4bf0eabef388c77d0f2ae0c3ce5
Ollie Hayman <discorev@olliespage.net>
2021-01-01 17:37:47 +0000

    Formatting

--- solver/cli/main.py
+++ solver/cli/main.py
@@ -6,13 +6,18 @@ from typing import Optional
 from solver.lib.collection import ContainerCollection
 from solver.lib.search import Option, bfs, dfs
 
+
 @click.command()
-@click.option('-a', '--algorithm',
-              type=click.Choice(['BFS', 'DFS'], case_sensitive=False),
-              default='BFS', show_default=True)
+@click.option(
+    "-a",
+    "--algorithm",
+    type=click.Choice(["BFS", "DFS"], case_sensitive=False),
+    default="BFS",
+    show_default=True,
+)
 @click.option("-v", "--verbose", is_flag=True)
 @click.argument("puzzle", type=click.File())
-def cli(puzzle=None, algorithm='BFS', verbose=False):
+def cli(puzzle=None, algorithm="BFS", verbose=False):
     """Solve PUZZLE.
 
     PUZZLE is the path to a json file describing the puzzle to solve.
@@ -26,11 +31,11 @@ def cli(puzzle=None, algorithm='BFS', verbose=False):
     print(start)
 
     result: Optional[Option] = None
-    if algorithm == 'BFS':
-        print('Searching using Breadth-First Search')
+    if algorithm == "BFS":
+        print("Searching using Breadth-First Search")
         result = bfs(start)
-    elif algorithm == 'DFS':
-        print('Searching using Depth-First Search')
+    elif algorithm == "DFS":
+        print("Searching using Depth-First Search")
         result = dfs(start)
 
     if result is None:
