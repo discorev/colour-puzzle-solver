6d15e00d640be2518fd13eb05681bad95ce50a48
Ollie Hayman <discorev@olliespage.net>
2021-02-08 20:20:38 +0000

    improved img2collection
    Better logic for removing invalid contours from the collections and moved colour mapping into it's own function that uses a more fuzzy approach.

--- solver/lib/img2collection.py
+++ solver/lib/img2collection.py
@@ -1,48 +1,19 @@
+"""Module for handling the conversion of images to collections."""
 import os
 import numpy as np
 from cv2 import cv2
-
+from typing import List
 
 from solver.lib.colour import Colour
 from solver.lib.item import Item
 from solver.lib.collection import ContainerCollection
 
-color_map = {
-    (181, 57, 45): Colour.RED,
-    (216, 103, 123): Colour.PINK,
-    (119, 75, 26): Colour.BROWN,
-    (127, 149, 48): Colour.GREEN,
-    (129, 212, 134): Colour.LIGHT_GREEN,
-    (46, 99, 55): Colour.DARK_GREEN,
-    (236, 218, 108): Colour.YELLOW,
-    (57, 46, 187): Colour.BLUE,
-    (103, 161, 224): Colour.LIGHT_BLUE,
-    (99, 100, 102): Colour.GREY,
-    (104, 47, 142): Colour.PURPLE,
-    (219, 143, 81): Colour.ORANGE,
-}
 
-
-def _sort_contours(contours, reverse=False):
-    def map_contour(cnt):
-        x, y, _, _ = cv2.boundingRect(cnt)
-        return x + (100 * y)
-
-    return sorted(contours, key=map_contour, reverse=reverse)
-
-
-def _rects_overlap(a, b):
-    x = max(a[0], b[0])
-    y = max(a[1], b[1])
-    w = min(a[0] + a[2], b[0] + b[2]) - x
-    h = min(a[1] + a[3], b[1] + b[3]) - y
-    if w < 0 or h < 0:
-        return False
-    return True
-
-
-def load(path):
+def load(path: str) -> ContainerCollection:
+    """Recognise an image file and create a `ContainerCollection`."""
     img = cv2.imread(path)
+    if img is None:
+        raise ValueError("Invalid file")
     tubes = _find_tubes_in_img(img)
     containers = []
     for tube in tubes:
@@ -53,11 +24,16 @@ def load(path):
     return ContainerCollection(containers)
 
 
-def _find_tubes_in_img(img):
+def _find_tubes_in_img(img: np.array):
+    """Find tubes in a loaded image.
+
+    This uses a nieve algorithm based on finding contours that cover a
+    large area of the image.
+    """
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray, 127, 255, 0)
     contours, _ = cv2.findContours(
-        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+        thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )
     tubes = _sort_contours(
         [cnt for cnt in contours if cv2.contourArea(cnt) > 60000]
@@ -65,12 +41,23 @@ def _find_tubes_in_img(img):
     return tubes
 
 
+def _sort_contours(contours: List[np.array], reverse=False):
+    """Sort a list of contours by the origin of their bounding rects."""
+
+    def map_contour(cnt: np.array) -> int:
+        x, y, _, _ = cv2.boundingRect(cnt)
+        return x + (100 * y)
+
+    return sorted(contours, key=map_contour, reverse=reverse)
+
+
 def _process_tube_from_img(img, tube):
-    # Extract the tube into an image
+    """Convert a tube image into a list of `Item` objects."""
     x, y, w, h = cv2.boundingRect(tube)
     tube_img = img[y : y + h, x : x + w]
+    threshold = 50
     # Use the Canny edge detector to find the items in the tube
-    canny = cv2.Canny(tube_img, 100, 200)
+    canny = cv2.Canny(tube_img, threshold, threshold * 3)
     # Threshold the canny result to make the items stand out
     tube_thresh = cv2.adaptiveThreshold(
         canny, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
@@ -79,34 +66,35 @@ def _process_tube_from_img(img, tube):
     contours, _ = cv2.findContours(
         tube_thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE
     )
-    # Filter items by area
+    # Filter for items within the right sort of area range and then let
+    # the filter below sort out which we really want
     items = [
         cnt
         for cnt in contours
-        if cv2.contourArea(cnt) > 8500 and cv2.contourArea(cnt) < 10100
+        if cv2.contourArea(cnt) > 8500 and cv2.contourArea(cnt) < 30000
     ]
+    # Remove overlapping elements that are larger in area to find the innermost
+    # contours within the tube as these are the insides of the coloured blocks
+    items = list(
+        filter(
+            lambda x: all(
+                [
+                    not (
+                        _rects_overlap(
+                            cv2.boundingRect(x), cv2.boundingRect(item)
+                        )
+                        and cv2.contourArea(x) > cv2.contourArea(item)
+                    )
+                    for item in items
+                    if not np.array_equal(x, item)
+                ]
+            ),
+            items,
+        )
+    )
 
-    # if there are not four items, then look for double sized
-    if len(items) < 4:
-        large_items = [
-            cnt
-            for cnt in contours
-            if cv2.contourArea(cnt) > 17000 and cv2.contourArea(cnt) < 21000
-        ]
-        for item in large_items:
-            item_rect = cv2.boundingRect(item)
-            # Ensure it dosen't overlap any other items
-            overlaps = False
-            for c in items:
-                c_rect = cv2.boundingRect(c)
-                if _rects_overlap(item_rect, c_rect):
-                    overlaps = True
-                    break
-            if not overlaps:
-                items.append(item)
-
-    # Draw the final contours onto the tube
-    if "DEBUG" in os.environ:
+    # When debugging draw the contours onto the tube
+    if "DEBUG" in os.environ:  # pragma: no cover
         tube_contours = tube_img.copy()
         tube_contours = cv2.drawContours(
             tube_contours, items, -1, (0, 255, 0), 3
@@ -118,9 +106,46 @@ def _process_tube_from_img(img, tube):
     for item in _sort_contours(items, reverse=True):
         x, y, w, h = cv2.boundingRect(item)
         sample = tube_img[y : y + 5, x : x + 5]
-        c = np.array(cv2.mean(sample)).astype(np.uint8)
-        item = Item(color_map.get((c[2], c[1], c[0])))
+        item = _item_for_sample(sample)
         container.append(item)
         if h > 100:
             container.append(item)
     return container
+
+
+def _rects_overlap(a: np.array, b: np.array):
+    """Check if two rects overlap by finding the intersection."""
+    x = max(a[0], b[0])
+    y = max(a[1], b[1])
+    w = min(a[0] + a[2], b[0] + b[2]) - x
+    h = min(a[1] + a[3], b[1] + b[3]) - y
+    if w < 0 or h < 0:
+        return False
+    return True
+
+
+def _item_for_sample(sample: np.array) -> Item:
+    """Convert the mean colour of a sample to an `Item`."""
+    color_map = [
+        (np.array([45, 57, 181, 0], dtype=np.uint8), Colour.RED),
+        (np.array([123, 103, 216, 0], dtype=np.uint8), Colour.PINK),
+        (np.array([26, 75, 119, 0], dtype=np.uint8), Colour.BROWN),
+        (np.array([48, 149, 127, 0], dtype=np.uint8), Colour.GREEN),
+        (np.array([134, 212, 129, 0], dtype=np.uint8), Colour.LIGHT_GREEN),
+        (np.array([55, 99, 46, 0], dtype=np.uint8), Colour.DARK_GREEN),
+        (np.array([108, 218, 236, 0], dtype=np.uint8), Colour.YELLOW),
+        (np.array([187, 46, 57, 0], dtype=np.uint8), Colour.BLUE),
+        (np.array([224, 161, 103, 0], dtype=np.uint8), Colour.LIGHT_BLUE),
+        (np.array([102, 100, 99, 0], dtype=np.uint8), Colour.GREY),
+        (np.array([142, 47, 104, 0], dtype=np.uint8), Colour.PURPLE),
+        (np.array([81, 143, 219, 0], dtype=np.uint8), Colour.ORANGE),
+    ]
+    c = np.array(cv2.mean(sample)).astype(np.uint8)
+    colour = None
+    for key, value in color_map:
+        if np.linalg.norm(key - c) < 2:
+            colour = value
+            break
+    if colour is None:
+        raise ValueError(f"Unknown Colour (R: {c[2]}, G: {c[1]}, B: {c[0]})")
+    return Item(colour)
